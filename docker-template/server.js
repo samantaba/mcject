63ef55a55d1abaf50ce5a5d42da4a80c3c4ff3a9
saman.tabatabaeian <saman.tabatabaeian@gmail.com>
2023-08-02 18:34:49 +1000

    tested a basic Hello World nodejs script

---
+++ docker-template/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", async (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.status(200);
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
